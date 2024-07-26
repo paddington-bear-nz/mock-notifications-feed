a5e297de2d9d27515fd42a4c811de708505ef12c
paddington-bear-nz <catto.enthusiastnz@gmail.com>
2024-07-26 19:55:36 +1200

    set up new vue vite app and install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
+<script setup>
+
+</script>
 <template>
-  <HelloWorld msg="Welcome to Your Mock Notification Feed"/>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
