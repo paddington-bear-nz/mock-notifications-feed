6596aa4a332e46a44e64daf6cc6a0be01245a729
paddington-bear-nz <catto.enthusiastnz@gmail.com>
2024-07-28 11:01:13 +1200

    add toast plugin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Notiwind from 'notiwind';
 
-createApp(App).mount('#app')
+const app= createApp(App);
+
+app.use(Notiwind);
+
+app.mount('#app');
