5100f1db593104fbf176b43aca7e6b640aabf916
paddington-bear-nz <catto.enthusiastnz@gmail.com>
2024-07-27 13:52:17 +1200

    style alignment, avatar and axios package install

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,79 @@
-<script setup>
-
-</script>
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="bg-gradient-to-b from-[#6b097a] to-[#274bdb] min-h-screen flex flex-col">
+
+    <header class="border-b-2 border-white border-opacity-50 py-5 sticky top-0">
+      <h2 class="text-xl font-semibold text-white text-center">Notifications</h2>
+    </header>
+    <div>
+      <ul role="list" class="divide-y divide-gray-500 divide-opacity-50">
+        <li v-for="notification in notifications" :key="notification.id" class="flex justify-between p-5">
+          <div class="flex flex-col gap-x-4 w-1/2 mx-auto">
+            <div class="flex items-center">
+              <span v-if="notification.read === false" class="inline-block h-1.5 w-1.5 bg-gray-300 rounded-full mr-2"></span>
+              <avatar :name="notification.author" :border="false"></avatar>
+              <div class="flex-auto ml-2">
+                <p class="text-sm font-semibold leading-6 text-white">{{ notification.title }}</p>
+                <div class="col-span-2">
+                  <p class="mt-1 text-xs leading-5 text-white">{{ notification.preview_text }}</p>
+                </div>
+                <div class="col-span-2">
+                  <p class="mt-1 text-xs leading-5 text-[#FFFFFF99]">
+                    {{ notification.author }}
+                    <span class="float-right">{{ notification.created }}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+  <footer class="bg-[#2f384c] opacity-75 sticky bottom-0">
+    <div class="w-1/2 mx-auto p-3">
+      <ul class="flex flex-wrap items-center justify-between text-sm font-medium text-gray-400">
+        <li>
+          <a href="#" class="hover:text-white">Home</a>
+        </li>
+        <li>
+          <a href="#" class="text-white">Notifications</a>
+        </li>
+        <li>
+          <a href="#" class="hover:text-white">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+
+
 </template>
+
+<script>
+import axios from "axios";
+import Avatar from "vue3-avatar";
+
+export default {
+  name: "Notifications",
+  components: {
+    Avatar
+  },
+  data() {
+    return {
+      notifications: []
+    };
+  },
+
+  async mounted() {
+    try {
+      let response = await axios.get("https://testau.asknice.ly/api/v1/candidate-test/mock-notifications");
+      this.notifications = response.data.data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+};
+</script>
 
 
